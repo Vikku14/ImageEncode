ff74e0d048c0b2e8eb5d4655e70d2a575b1436a9
vikku14 <sharmavikku14@gmail.com>
2020-06-15 08:17:34 +0530

    updated paths

--- Encode/views.py
+++ Encode/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def picture(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'Encode/picture.html')
