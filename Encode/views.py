86331191c0d0192f7d13013abf5e4dcb0931fd11
vikku14 <sharmavikku14@gmail.com>
2020-06-16 15:41:16 +0530

    restructured

--- Encode/views.py
+++ Encode/views.py
@@ -4,17 +4,10 @@ from django.conf import settings
 from base64 import b64encode
 from hashlib import md5
 from os import path
-from rest_framework import status
 from .forms import ImageForm
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import JsonResponse
-from .serializers import ImageSerializer, ImagePostSerializer
+
 from .models import ImageData
-from rest_framework.parsers import FileUploadParser
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-from rest_framework.views import APIView
-from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 def picture(request):
     if request.method == "POST":
@@ -37,34 +30,3 @@ def picture(request):
             return render(request, 'Encode/picture.html', {'form': form})
     else:
         return render(request, 'Encode/picture.html', {'form': ImageForm()})
-
-
-
-class ImageApi(APIView):
-    parser_class = (FileUploadParser,)
-
-    def get(self, request):
-        images = ImageData.objects.all()
-        ser = ImageSerializer(images, many= True)
-        return Response(ser.data)
-
-    @csrf_exempt
-    def post(self, request):
-        serializer = ImageSerializer(data = request.data)
-
-        if serializer.is_valid():
-            try:
-                pic = request.data['photo']
-                pic_read = pic.read()
-                pic_readed = b64encode(pic_read)
-                hash_result = md5(pic_read)                 # md5
-                serializer.save(photo = pic)
-                                #convert into bite string
-                serializer.save(base64_format = pic_readed.decode('ascii'), hash_format=hash_result.hexdigest() )
-
-                return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
-
-            except Exception as e:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- ImgEncode/urls.py
+++ ImgEncode/urls.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 
-from Encode import api_views
+
 from Encode import views
-
+from Encode.api import views as vs
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Encode.urls')),
-    # path('api/', views.apiOverview, name = 'overview'),
-
-
-    path('api/image-api', views.ImageApi.as_view(), name= 'image-api'),
+    path('api/image-api', vs.ImageApi.as_view(), name= 'image-api'),
 
 ]
